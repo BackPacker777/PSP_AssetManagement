46043c7c54edcd990ed7d868f1151300ba1ccbc9
Howard Bates <hbates@northmen.org>
2018-07-30 13:54:43 -0400

    Working on readme.md work & Heroku pushing.....

--- bin/minify.bat
+++ bin/minify.bat
@@ -1,4 +1,4 @@
-uglifyjs public/javascripts/main.js --compress -o public/javascripts/main.min.js
-uglifyjs public/javascripts/BCScan.js --compress -o public/javascripts/BCScan.min.js
-uglifyjs public/javascripts/EventHandler.js --compress -o public/javascripts/EventHandler.min.js
-uglifyjs public/javascripts/SplashEventHandler.js --compress -o public/javascripts/SplashEventHandler.min.js
+start uglifyjs public/javascripts/main.js --compress -o public/javascripts/main.min.js
+start uglifyjs public/javascripts/BCScan.js --compress -o public/javascripts/BCScan.min.js
+start uglifyjs public/javascripts/EventHandler.js --compress -o public/javascripts/EventHandler.min.js
+start uglifyjs public/javascripts/SplashEventHandler.js --compress -o public/javascripts/SplashEventHandler.min.js
